ade72949b6b6ee707b099ee5a3f571a6f9d976ef
Emma Forsberg <32894170+EmmaForsberg@users.noreply.github.com>
2018-01-09 13:31:15 +0100

    Lagt till två metoder i SaveGame. Dock ej klara ännu.

--- GOL_V01/SaveGame.cs
+++ GOL_V01/SaveGame.cs
@@ -8,6 +8,36 @@ namespace GOL
 {
     public class SaveGame
     {
+        //två funktioner, en för att spara spel och en för att spara varje runda.
+        Settings s;
+
+        public void IfSaveGame(string SaveName)//spara spel
+        {
+            using (var context = new DBContext())
+            {
+                var newsave = new Game();
+                newsave.SaveName = "BestGame";
+                newsave.SaveDate = System.DateTime.Now;
+                context.Games.Add(newsave);
+            }
+            return;
+        }
+
+        public void IfSaveRounds(string y)//save rounds
+        {
+            using (var context = new DBContext())
+            {
+                var newround = new GameRound();
+                
+                newround.GridSize = s.GridSize;
+                newround.PlayingField = currentroundstring;
+                newround.Round = 1;
+           
+                context.Rounds.Add(newround);
+                context.SaveChanges();
+            }
         
+        }
+
     }
 }
